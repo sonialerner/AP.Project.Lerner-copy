e26e3c3d14182f45f7e775bb8a6ab8e79a37b16f
sonialerner <sonialerner05@gmail.com>
2023-03-13 08:27:42 -0500

    Initial Commit

---
+++ AP.Project.Lerner/AP_Project_LernerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_Project_LernerApp.swift
+//  AP.Project.Lerner
+//
+//  Created by Sonia Lerner24 on 3/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AP_Project_LernerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
